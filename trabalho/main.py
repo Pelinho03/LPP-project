507be03649dd6943e6f7e1204bda8d5a4128ac4c
Paulo Guimarães <elmuchacho@outlook.pt>
2025-04-24 22:23:48 +0100

    refactor/feat
    - Código limpo, pastas criadas e nomes dos scripts alterados para fácil manutenção
    - LoginUI e UserUI criados com ramificação via email
    etc.

--- trabalho/main.py
+++ trabalho/main.py
@@ -1,18 +1,24 @@
 # Arquivo principal
 import sys
 from PyQt6.QtWidgets import QApplication
-from ui import GestorTarefas
-from login import Window
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    gestor = GestorTarefas()
-    gestor.show()
-    sys.exit(app.exec())
-
+from ui.adminUI import GestorTarefas
+from ui.loginUI import LoginUI
+from ui.userUI import UserUI
 
 # if __name__ == "__main__":
 #     app = QApplication(sys.argv)
-#     window = Window()
-#     window.show()
+#     gestor = GestorTarefas()
+#     gestor.show()
+#     sys.exit(app.exec())
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    login = LoginUI()
+    login.show()
+    sys.exit(app.exec())
+
+# if __name__ == "__main__":
+#     app = QApplication(sys.argv)
+#     userUI = UserUI()
+#     userUI.show()
 #     sys.exit(app.exec())
